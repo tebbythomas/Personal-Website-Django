b13f8893ac57fc293754051a3a9bd61a0b3a590b
Tebby Thomas <tebby.thomas@gmail.com>
2020-03-11 13:14:45 +0530

    Adding remaining files

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l0yj+)pfrnaxad-)e!k6bj9$j_cj0uev@_e*7cl$8izp9li728'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "192.168.1.6", "127.0.0.1"]
 
 
 # Application definition
